28490635a3907b5f27ae59365d3269f93ad9eae8
Tomasz Borek <tomasz.borek.89@gmail.com>
2023-04-18 22:40:03 +0200

    Done of homework no. 6

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('#validation-input');
+const inputDataLength = input.getAttribute("data-length");
+
+const blur = () => {
+    if(input.value.length < inputDataLength){
+        input.classList.add("invalid");
+    }
+    else{
+        input.classList.add("valid");
+    }
+}
+
+input.addEventListener('blur', blur);
+
