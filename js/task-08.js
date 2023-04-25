243b75bfbf5c38a27a213d71a98b3559c0b74b8f
Tomasz Borek <tomasz.borek.89@gmail.com>
2023-04-25 21:41:15 +0200

    Done of task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,7 +1,19 @@
 const form = document.querySelector('.login-form');
+const {elements: {email, password} } = form;
 
-function handleSubmit(event){
+const handleSubmit = (event) => {
     event.preventDefault();
+
+    if(email.value === ""){
+        email.value = "Please complate the field of form";
+    }
+    if(password.value === ""){
+        password.value = "Please complate the field of form";
+    }
+console.log(event.currentTarget);
+    
+console.log(` Email: ${email.value} \n password: ${password.value}`);
+event.currentTarget.reset();
 }
 
-form.addEventListener("submit", handleSubmit);
+form.addEventListener("submit", handleSubmit);  
